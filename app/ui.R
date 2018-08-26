0e40d3378461470d7cb4a3fe4e5d779c581ccf87
Brendan Molin - PC <brendan.molin@gmail.com>
2018-08-26 18:51:12 -0400

    swap plot place

--- app/ui.R
+++ app/ui.R
@@ -1,13 +1,19 @@
 pageWithSidebar(
+  # HEADER AND DETAILS
   headerPanel('Oh, the Places You\'ll (Probably) Go'),
+  # USER INPUT
   sidebarPanel(
+    # Which schools you're applying to, in order of preference
     selectInput(inputId = 'schools', label = 'List of Schools to Apply to (in order of preference)',
                 choices = sort(dir$inst_name), selected = c("Northeastern University", "Fordham University", "University of Miami", "George Mason University"),
                 multiple = TRUE),
     actionButton("go", "Apply")
   ),
+  # OUTPUT
   mainPanel(
+    # Probability of getting admitted; updates with each change to input 'schools'
     plotOutput('prop_plot'),
+    # Probability of going to each school; updates after action button
     plotOutput('sim_plot')
   )
 )
